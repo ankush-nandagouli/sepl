a68f773a30d21360816292d5ef67dbbf3a897861
Ankush Nandagouli <ankush2004nanda@gmail.com>
2025-11-30 17:00:13 +0530

    command commit

--- sepl_project/urls.py
+++ sepl_project/urls.py
@@ -3,12 +3,21 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.core.management import call_command
+from django.http import HttpResponse
+
+def run_migrations(request):
+    call_command("migrate")
+    return HttpResponse("Migrations applied!")
 
 urlpatterns = [
-    
+    path("run-migrate/", run_migrations),
     path('', include('auction.urls')),
     path('admin/', admin.site.urls),
+
+    
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
